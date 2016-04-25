6cefe411de0d0be0a89d17357c99ac20170ba1a8
Rainer Kuhn <ticktricktrack@gmail.com>
2016-04-25 10:55:08 +0100

    add numbers back to layer 0

--- keyboard/ergodox_ez/keymaps/workman_rainer/keymap.c
+++ keyboard/ergodox_ez/keymaps/workman_rainer/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_TRNS,
                                       KC_TRNS, KC_TRNS, KC_TRNS,
         // right hand
-           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_TRNS,  KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
            KC_TRNS,  KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS,
                      KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS,
            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
